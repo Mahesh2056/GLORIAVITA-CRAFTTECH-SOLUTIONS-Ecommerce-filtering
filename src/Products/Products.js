ab7ce50d0db66cad295529294328c694967f258a
MAHESH2056 <maheschy2323@gmail.com>
2024-07-25 18:25:46 +0545

    Card Component Refactor

--- src/Products/Products.js
+++ src/Products/Products.js
@@ -1,21 +1,11 @@
-import Card from "../components/Card";
 import "./Product.css";
 
-function Products() {
+function Products({ result }) {
   return (
     <>
-     <section className="card-container">
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-      <Card />
-     </section>
+     <section className="card-container">{result}</section>
     </>
-  )
+  );
 };
 
 export default Products;
